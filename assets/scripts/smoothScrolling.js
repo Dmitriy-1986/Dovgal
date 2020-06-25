718c76ad43b6bc75d9776b78459b0ee9dc98eb37
Dmitriy-1986 <52016913+Dmitriy-1986@users.noreply.github.com>
2020-06-25 07:38:17 +0300

    Update smoothScrolling.js

--- assets/scripts/smoothScrolling.js
+++ assets/scripts/smoothScrolling.js
@@ -14,7 +14,7 @@ const scrollToTop = () => {
   const c = document.documentElement.scrollTop || document.body.scrollTop;
   if (c > 0) {
     window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, c - c / 20);
+    window.scrollTo(0, c - c / 10);
   }
 };
 
