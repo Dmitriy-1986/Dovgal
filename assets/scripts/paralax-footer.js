a1368cb2aa1ca6ad85b020c0b51cb6d6cef02d94
Dmitriy-1986 <52016913+Dmitriy-1986@users.noreply.github.com>
2020-08-06 00:13:29 +0300

    Update paralax-footer.js

--- assets/scripts/paralax-footer.js
+++ assets/scripts/paralax-footer.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 		console.log('Footer Width = ' + siteFooterWidth + 'px');
 
 		siteContent.css({
-			"margin-bottom" : siteFooterHeight + 40
+			"margin-bottom" : siteFooterHeight + 0
 		});
 	};
 });
