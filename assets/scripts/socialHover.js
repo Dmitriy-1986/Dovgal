1cd25da8908e51ce73dc4c27865372915c7bf84e
Dmitriy-1986 <52016913+Dmitriy-1986@users.noreply.github.com>
2020-08-14 10:01:14 +0300

    Add files via upload

---
+++ assets/scripts/socialHover.js
@@ -0,0 +1,329 @@
+/* Facebook */
+facebookLink.onmouseover = function () {
+    let blok = document.getElementById('facebook');
+    blok.style.borderColor = "#4267b2";
+    blok.style.boxShadow = "0px 0px 20px #4267b2";
+    blok.style.textShadow = "0px 0px 20px #4267b2";
+    blok.style.color = "#4267b2";
+    
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('facebookLink');
+    black.style.textShadow = "0px 0px 20px #4267b2";
+    black.style.color = "#4267b2";
+}
+
+facebookLink.onmouseout = function () {
+    let blok = document.getElementById('facebook');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('facebookLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+facebook.onmouseover = function () {
+    let blok = document.getElementById('facebook');
+    blok.style.borderColor = "#4267b2";
+    blok.style.boxShadow = "0px 0px 20px #4267b2";
+    blok.style.textShadow = "0px 0px 20px #4267b2";
+    blok.style.color = "#4267b2";
+
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('facebookLink');
+    black.style.textShadow = "0px 0px 20px #4267b2";
+    black.style.color = "#4267b2";
+}
+
+facebook.onmouseout = function () {
+    let blok = document.getElementById('facebook');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('facebookLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+/* Twitter */
+twitterLink.onmouseover = function () {
+    let blok = document.getElementById('twitter');
+    blok.style.borderColor = "#1DA1F2";
+    blok.style.boxShadow = "0px 0px 20px #1DA1F2";
+    blok.style.textShadow = "0px 0px 20px #1DA1F2";
+    blok.style.color = "#1DA1F2";
+    
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('twitterLink');
+    black.style.textShadow = "0px 0px 20px #1DA1F2";
+    black.style.color = "#1DA1F2";
+}
+
+twitterLink.onmouseout = function () {
+    let blok = document.getElementById('twitter');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('twitterLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+twitter.onmouseover = function () {
+    let blok = document.getElementById('twitter');
+    blok.style.borderColor = "#1DA1F2";
+    blok.style.boxShadow = "0px 0px 20px #1DA1F2";
+    blok.style.textShadow = "0px 0px 20px #1DA1F2";
+    blok.style.color = "#1DA1F2";
+
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('twitterLink');
+    black.style.textShadow = "0px 0px 20px #1DA1F2";
+    black.style.color = "#1DA1F2";
+}
+
+twitter.onmouseout = function () {
+    let blok = document.getElementById('twitter');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('twitterLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+/* Instagram */
+instagramLink.onmouseover = function () {
+    let blok = document.getElementById('instagram');
+    blok.style.borderColor = "#C13584";
+    blok.style.boxShadow = "0px 0px 20px #C13584";
+    blok.style.textShadow = "0px 0px 20px #C13584";
+    blok.style.color = "#C13584";
+    
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('instagramLink');
+    black.style.textShadow = "0px 0px 20px #C13584";
+    black.style.color = "#C13584";
+}
+
+instagramLink.onmouseout = function () {
+    let blok = document.getElementById('instagram');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('instagramLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+instagram.onmouseover = function () {
+    let blok = document.getElementById('instagram');
+    blok.style.borderColor = "#C13584";
+    blok.style.boxShadow = "0px 0px 20px #C13584";
+    blok.style.textShadow = "0px 0px 20px #C13584";
+    blok.style.color = "#C13584";
+
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('instagramLink');
+    black.style.textShadow = "0px 0px 20px #C13584";
+    black.style.color = "#C13584";
+}
+
+instagram.onmouseout = function () {
+    let blok = document.getElementById('instagram');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('instagramLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+} 
+
+/* Skype */
+skypeLink.onmouseover = function () {
+    let blok = document.getElementById('skype');
+    blok.style.borderColor = "#00AFF0";
+    blok.style.boxShadow = "0px 0px 20px #00AFF0";
+    blok.style.textShadow = "0px 0px 20px #00AFF0";
+    blok.style.color = "#00AFF0";
+    
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('skypeLink');
+    black.style.textShadow = "0px 0px 20px #00AFF0";
+    black.style.color = "#00AFF0";
+}
+
+skypeLink.onmouseout = function () {
+    let blok = document.getElementById('skype');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('skypeLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+skype.onmouseover = function () {
+    let blok = document.getElementById('skype');
+    blok.style.borderColor = "#00AFF0";
+    blok.style.boxShadow = "0px 0px 20px #00AFF0";
+    blok.style.textShadow = "0px 0px 20px #00AFF0";
+    blok.style.color = "#00AFF0";
+
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('skypeLink');
+    black.style.textShadow = "0px 0px 20px #00AFF0";
+    black.style.color = "#00AFF0";
+}
+
+skype.onmouseout = function () {
+    let blok = document.getElementById('skype');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('skypeLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+} 
+
+/* Telegram */
+telegramLink.onmouseover = function () {
+    let blok = document.getElementById('telegram');
+    blok.style.borderColor = "#0088cc";
+    blok.style.boxShadow = "0px 0px 20px #0088cc";
+    blok.style.textShadow = "0px 0px 20px #0088cc";
+    blok.style.color = "#0088cc";
+    
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('telegramLink');
+    black.style.textShadow = "0px 0px 20px #0088cc";
+    black.style.color = "#0088cc";
+}
+
+telegramLink.onmouseout = function () {
+    let blok = document.getElementById('telegram');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('telegramLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+telegram.onmouseover = function () {
+    let blok = document.getElementById('telegram');
+    blok.style.borderColor = "#0088cc";
+    blok.style.boxShadow = "0px 0px 20px #0088cc";
+    blok.style.textShadow = "0px 0px 20px #0088cc";
+    blok.style.color = "#0088cc";
+
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('telegramLink');
+    black.style.textShadow = "0px 0px 20px #0088cc";
+    black.style.color = "#0088cc";
+}
+
+telegram.onmouseout = function () {
+    let blok = document.getElementById('telegram');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('telegramLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+} 
+
+/* Linkedin */
+linkedinLink.onmouseover = function () {
+    let blok = document.getElementById('linkedin');
+    blok.style.borderColor = "#2867B2";
+    blok.style.boxShadow = "0px 0px 20px #2867B2";
+    blok.style.textShadow = "0px 0px 20px #2867B2";
+    blok.style.color = "#2867B2";
+    
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('linkedinLink');
+    black.style.textShadow = "0px 0px 20px #2867B2";
+    black.style.color = "#2867B2";
+}
+
+linkedinLink.onmouseout = function () {
+    let blok = document.getElementById('linkedin');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('linkedinLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+}
+
+linkedin.onmouseover = function () {
+    let blok = document.getElementById('linkedin');
+    blok.style.borderColor = "#2867B2";
+    blok.style.boxShadow = "0px 0px 20px #2867B2";
+    blok.style.textShadow = "0px 0px 20px #2867B2";
+    blok.style.color = "#2867B2";
+
+    blok.style.transition = '2s';
+
+    let black = document.getElementById('linkedinLink');
+    black.style.textShadow = "0px 0px 20px#2867B2";
+    black.style.color = "#2867B2";
+}
+
+linkedin.onmouseout = function () {
+    let blok = document.getElementById('linkedin');
+    blok.style.borderColor = '#66696c';
+    blok.style.color = '#66696c';
+    blok.style.boxShadow = "none";
+    blok.style.textShadow = "none";
+
+    let black = document.getElementById('linkedinLink');
+    black.style.color = '#66696c';
+    black.style.boxShadow = "none";
+    black.style.textShadow = "none";
+} 
