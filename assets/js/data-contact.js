172f5ef94097b3411b3d7b79c31469ce67b84341
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-07-15 04:13:00 +0300

    Update data-contact.js

--- assets/js/data-contact.js
+++ assets/js/data-contact.js
@@ -19,6 +19,6 @@ let socialMediaList = document.createElement('li');
 myContact.append(socialMedia);
 socialMedia.append(socialMediaList);
 
-socialMediaList.innerHTML  = `<a href="${dataContact.facebook.url}" title="${dataContact.facebook.title}">${dataContact.facebook.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.twitter.url}" title="${dataContact.twitter.title}">${dataContact.twitter.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.instagram.url}" title="${dataContact.instagram.title}">${dataContact.instagram.title}</a>`;
+socialMediaList.innerHTML  = `<a href="${dataContact.facebook.url}" title="${dataContact.facebook.title}" class="social-media">${dataContact.facebook.title}</a>`;
+socialMediaList.innerHTML += `<a href="${dataContact.twitter.url}" title="${dataContact.twitter.title}" class="social-media">${dataContact.twitter.title}</a>`;
+socialMediaList.innerHTML += `<a href="${dataContact.instagram.url}" title="${dataContact.instagram.title}" class="social-media">${dataContact.instagram.title}</a>`;
