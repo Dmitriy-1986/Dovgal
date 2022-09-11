4530f576f1a85c087d34fe359cab6c431350b2b0
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-09-11 13:09:27 +0300

    Update data-contact.js

--- assets/js/data-contact.js
+++ assets/js/data-contact.js
@@ -1,18 +1,19 @@
 let dataContact = {
   facebook: {
     title: 'Facebook',
-    //url: 'javascript:void(0)'
     url: 'https://www.facebook.com/dovgaldima'
   },
   twitter: {
     title: 'Twitter',
-    //url: 'javascript:void(0)'
     url: 'https://twitter.com/DovgalDmitriy'
   },
   instagram: {
     title: 'Instagram',
-    //url: 'javascript:void(0)'
     url: 'https://www.instagram.com/dmitriy_dovgal'
+  },
+  linkedin: {
+     title: 'LinkedIn',
+     url: 'https://www.linkedin.com/mwlite/in/dmitriy-dovgal-2672141a2'
   },
 };
 
@@ -25,6 +26,7 @@ socialMedia.append(socialMediaList);
 socialMediaList.innerHTML  = `<a href="${dataContact.facebook.url}" title="${dataContact.facebook.title}" class="social-media">${dataContact.facebook.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.twitter.url}" title="${dataContact.twitter.title}" class="social-media">${dataContact.twitter.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.instagram.url}" title="${dataContact.instagram.title}" class="social-media">${dataContact.instagram.title}</a>`;
+socialMediaList.innerHTML += `<a href="${dataContact.linkedin.url}" title="${dataContact.linkedin.title}" class="social-media">${dataContact.linkedin.title}</a>`;
 socialMediaList.innerHTML += `<a href="mailto: dovgal.dima.86@gmail.com?subject=Feedback&body=Message:">
                                 <button type="button" class="btn btn-light">                     
                                   Send Feedback 
