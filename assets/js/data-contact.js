630588d2235e4f3e78b77cd05211881173edabb5
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-11-20 14:44:25 +0200

    Update data-contact.js

--- assets/js/data-contact.js
+++ assets/js/data-contact.js
@@ -28,7 +28,7 @@ socialMediaList.innerHTML  = `<a href="${dataContact.facebook.url}" title="${dat
 socialMediaList.innerHTML += `<a href="${dataContact.twitter.url}" title="${dataContact.twitter.title}" class="social-media">${dataContact.twitter.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.instagram.url}" title="${dataContact.instagram.title}" class="social-media">${dataContact.instagram.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.linkedin.url}" title="${dataContact.linkedin.title}" class="social-media">${dataContact.linkedin.title}</a>`;
-socialMediaList.innerHTML += `<a href="https://ru.stackoverflow.com/users/441480/dovgal-dima">
+socialMediaList.innerHTML += `<a href="https://ru.stackoverflow.com/users/441480/dovgal-dima" style="margin-top: 10px;">
                                 <img src="https://ru.stackoverflow.com/users/flair/441480.png?theme=dark" width="208" height="58" alt="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов" title="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов">
                               </a>`;
 socialMediaList.innerHTML += `<a href="mailto: dovgal.dima.86@gmail.com?subject=Feedback&body=Message:">
