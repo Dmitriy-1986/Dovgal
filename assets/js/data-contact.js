e62979c825ec9de0b93ac98c07dc79c71a495780
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-11-20 14:12:44 +0200

    Update data-contact.js

--- assets/js/data-contact.js
+++ assets/js/data-contact.js
@@ -27,7 +27,7 @@ socialMedia.append(socialMediaList);
 socialMediaList.innerHTML  = `<a href="${dataContact.facebook.url}" title="${dataContact.facebook.title}" class="social-media">${dataContact.facebook.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.twitter.url}" title="${dataContact.twitter.title}" class="social-media">${dataContact.twitter.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.instagram.url}" title="${dataContact.instagram.title}" class="social-media">${dataContact.instagram.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.linkedin.url}" title="${dataContact.linkedin.title}" class="social-media">${dataContact.linkedin.title}</a>`;
+socialMediaList.innerHTML += `<a href="${dataContact.linkedin.url}" title="${dataContact.linkedin.title}" class="social-media">${dataContact.linkedin.title}</a><br>`;
 socialMediaList.innerHTML += `<a href="https://ru.stackoverflow.com/users/441480/dovgal-dima">
                                 <img src="https://ru.stackoverflow.com/users/flair/441480.png?theme=dark" width="208" height="58" alt="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов" title="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов">
                               </a>`;
