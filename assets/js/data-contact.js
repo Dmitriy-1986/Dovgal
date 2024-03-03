abaa7aab88609c69c4038c30725626481d24f850
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2024-03-03 14:22:31 +0200

    Update data-contact.js

--- assets/js/data-contact.js
+++ assets/js/data-contact.js
@@ -42,11 +42,11 @@ socialMediaList.innerHTML += `<a href="${dataContact.ok.url}" title="${dataConta
 socialMediaList.innerHTML += `<a href="${dataContact.twitter.url}" title="${dataContact.twitter.title}" class="social-media">${dataContact.twitter.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.instagram.url}" title="${dataContact.instagram.title}" class="social-media">${dataContact.instagram.title}</a>`;
 socialMediaList.innerHTML += `<a href="${dataContact.telegram.url}" title="${dataContact.telegram.title}" class="social-media">${dataContact.telegram.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.linkedin.url}" title="${dataContact.linkedin.title}" class="social-media">${dataContact.linkedin.title}</a><br><br>`;
-socialMediaList.innerHTML += `<br><br><a href="https://ru.stackoverflow.com/users/441480/dovgal-dima" style="margin-top: 10px; text-decoration: none;">
+socialMediaList.innerHTML += `<a href="${dataContact.linkedin.url}" title="${dataContact.linkedin.title}" class="social-media">${dataContact.linkedin.title}</a>`;
+socialMediaList.innerHTML += `<div><a href="https://ru.stackoverflow.com/users/441480/dovgal-dima" style="margin-top: 10px; text-decoration: none;">
                                 <img src="https://ru.stackoverflow.com/users/flair/441480.png?theme=dark" width="208" height="58" alt="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов" title="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов">
-                              </a>`;
-socialMediaList.innerHTML += `<br><br><a href="https://www.codewars.com/users/Dovgal_Dima"><img src="https://www.codewars.com/users/Dovgal_Dima/badges/small" alt="Codewars Badge"></a>`;
+                              </a></div>`;
+socialMediaList.innerHTML += `<div><a href="https://www.codewars.com/users/Dovgal_Dima"><img src="https://www.codewars.com/users/Dovgal_Dima/badges/small" alt="Codewars Badge"></a></div>`;
 socialMediaList.innerHTML += `<a href="mailto: dovgal.dima.86@gmail.com?subject=Feedback&body=Message:">
                                 <button type="button" class="btn btn-light">                     
                                   Send Feedback 
