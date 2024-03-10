f1865c1ff57e4ab70f140d091e9d9751079f0298
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2024-03-10 21:06:41 +0200

    Update data-about-me.js

--- assets/js/data-about-me.js
+++ assets/js/data-about-me.js
@@ -1,16 +1,32 @@
-let stringsArray = [' JavaScript', ' React.js', ' Bootstrap', ' PHP', ' MySQL'];
-
-let showMessage = (name, courses, ...rest) => {
-    return `My name is ${name}. I finished ${courses}. I've learned ${rest}...`;       
-}
-
-let aboutMe = document.querySelector('#about-me');
-let p = document.createElement('p');
-p.innerHTML = showMessage('Dima', 'ItStep', ...stringsArray);
-aboutMe.append(p);
-
-let linkGitHub = document.createElement('a');
-linkGitHub.classList.add('social-media');
-linkGitHub.setAttribute('href', 'https://github.com/Dmitriy-1986');
-linkGitHub.innerHTML = 'GitHub';
-aboutMe.append(linkGitHub);
+const myInfo = (() => {
+    const stringsArray = [' JavaScript', ' React.js', ' Bootstrap', ' PHP', ' MySQL'];
+  
+    const showMessage = (name, courses, ...rest) => {
+      return `My name is ${name}. I finished ${courses}. I've learned ${rest.join(', ')}...`;       
+    };
+  
+    const renderAboutMe = () => {
+      const aboutMe = document.querySelector('#about-me');
+      
+      const p = document.createElement('p');
+      p.innerHTML = showMessage('Dima', 'ItStep', ...stringsArray);
+      aboutMe.append(p);
+  
+      const linkGitHub = document.createElement('a');
+      linkGitHub.classList.add('social-media');
+      linkGitHub.setAttribute('href', 'https://github.com/Dmitriy-1986');
+      linkGitHub.innerHTML = 'GitHub';
+      aboutMe.append(linkGitHub);
+    };
+  
+    return {
+      init: () => {
+        document.addEventListener('DOMContentLoaded', () => {
+          renderAboutMe();
+        });
+      }
+    };
+})();
+  
+myInfo.init();
+  
