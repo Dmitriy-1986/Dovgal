1a51b6e520d616d0891a08e54999e0bbf952c5b1
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2024-03-10 20:56:10 +0200

    Update data-contact.js

--- assets/js/data-contact.js
+++ assets/js/data-contact.js
@@ -1,62 +1,84 @@
-let dataContact = {
-  facebook: {
-    title: 'Facebook',
-    url: 'https://www.facebook.com/dovgaldima'
-  },
-  vk: {
-    title: 'VK',
-    url: 'https://m.vk.com/id790847045'
-  },
-  ok: {
-    title: 'OK',
-    url: 'https://ok.ru/profile/582209131417'
-  },
-  twitter: {
-    title: 'Twitter',
-    url: 'https://twitter.com/DovgalDmitriy'
-  },
-  instagram: {
-    title: 'Instagram',
-    url: 'https://www.instagram.com/dmitriy_dovgal'
-  },
-  telegram: {
-    title: 'Telegram',
-    url: 'https://t.me/DovgalDima'
-  },
-  linkedin: {
-     title: 'LinkedIn',
-     url: 'https://www.linkedin.com/in/dmitriy-dovgal-2672141a2/'
-  },
-};
+(function() {
+  const elementContact = document.querySelector('#my-contact');
+  const myContact = {
+    socialMedia: {
+      data: {
+        facebook: {
+          title: 'Facebook',
+          url: 'https://www.facebook.com/dovgaldima'
+        },
+        vk: {
+          title: 'VK',
+          url: 'https://m.vk.com/id790847045'
+        },
+        ok: {
+          title: 'OK',
+          url: 'https://ok.ru/profile/582209131417'
+        },
+        twitter: {
+          title: 'Twitter',
+          url: 'https://twitter.com/DovgalDmitriy'
+        },
+        instagram: {
+          title: 'Instagram',
+          url: 'https://www.instagram.com/dmitriy_dovgal'
+        },
+        telegram: {
+          title: 'Telegram',
+          url: 'https://t.me/DovgalDima'
+        },
+        linkedin: {
+          title: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/dmitriy-dovgal-2672141a2/'
+        }
+      },
+      render: function () {
+        let socialMedia = document.createElement('div');
+        let socialMediaList = document.createElement('p');
+        socialMediaList.classList.add('social-links-container');
+        elementContact.append(socialMedia);
+        socialMedia.append(socialMediaList);
 
-let myContact = document.querySelector('#my-contact');
-let socialMedia = document.createElement('div');
-let socialMediaList = document.createElement('p');
-socialMediaList.classList.add('social-links-container');
-myContact.append(socialMedia);
-socialMedia.append(socialMediaList);
+        for (let key in this.data) {
+          socialMediaList.innerHTML += `<a href="${this.data[key].url}" title="${this.data[key].title}" class="social-media">${this.data[key].title}</a>`;
+        }
+      }
+    },
+    stackoverflow: {
+      url: 'https://ru.stackoverflow.com/users/441480/dovgal-dima',
+      img: 'https://ru.stackoverflow.com/users/flair/441480.png?theme=dark',
+      render: function () {
+        elementContact.innerHTML += `<p><a href="${this.url}" style="margin-top: 10px; text-decoration: none;">
+                                      <img src="${this.img}" width="208" height="58" alt="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов" title="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов">
+                                    </a></p>`;
+      }
+    },
+    codewars: {
+      url: 'https://www.codewars.com/users/Dovgal_Dima',
+      badge: 'https://www.codewars.com/users/Dovgal_Dima/badges/small',
+      render: function () {
+        elementContact.innerHTML += `<p><a href="${this.url}"><img src="${this.badge}" alt="Codewars Badge"></a></p>`;
+      }
+    },
+    feedback: {
+      email: 'dovgal.dima.86@gmail.com',
+      render: function () {
+        elementContact.innerHTML += `<a href="mailto: ${this.email}?subject=Feedback&body=Message:">
+                                      <button type="button" class="btn btn-light">
+                                        Send Feedback 
+                                      </button>
+                                    </a>`;
+      }
+    },
+    init: function () {
+      this.socialMedia.render();
+      this.stackoverflow.render();
+      this.codewars.render();
+      this.feedback.render();
+    }
+  };
 
-socialMediaList.innerHTML = `<div>`;
-socialMediaList.innerHTML += `<a href="${dataContact.facebook.url}" title="${dataContact.facebook.title}" class="social-media">${dataContact.facebook.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.vk.url}" title="${dataContact.vk.title}" class="social-media">${dataContact.vk.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.ok.url}" title="${dataContact.ok.title}" class="social-media">${dataContact.ok.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.twitter.url}" title="${dataContact.twitter.title}" class="social-media">${dataContact.twitter.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.instagram.url}" title="${dataContact.instagram.title}" class="social-media">${dataContact.instagram.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.telegram.url}" title="${dataContact.telegram.title}" class="social-media">${dataContact.telegram.title}</a>`;
-socialMediaList.innerHTML += `<a href="${dataContact.linkedin.url}" title="${dataContact.linkedin.title}" class="social-media">${dataContact.linkedin.title}</a><br><br>`;
-socialMediaList.innerHTML += `</div><br>`;
-socialMediaList.innerHTML += `<p><a href="https://ru.stackoverflow.com/users/441480/dovgal-dima" style="margin-top: 10px; text-decoration: none;">
-                                <img src="https://ru.stackoverflow.com/users/flair/441480.png?theme=dark" width="208" height="58" alt="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов" title="Профиль участника Dovgal Dima на сайте &#171;Stack Overflow на русском&#187;, Вопросы и ответы для программистов">
-                              </a></p>`;
-socialMediaList.innerHTML += `<p><a href="https://www.codewars.com/users/Dovgal_Dima"><img src="https://www.codewars.com/users/Dovgal_Dima/badges/small" alt="Codewars Badge"></a></p>`;
-socialMediaList.innerHTML += `<a href="mailto: dovgal.dima.86@gmail.com?subject=Feedback&body=Message:">
-                                <button type="button" class="btn btn-light">                     
-                                  Send Feedback 
-                                </button>
-                              </a>`;
-
-
-
-
-
-
+  document.addEventListener('DOMContentLoaded', function () {
+    myContact.init();
+  });
+})();
